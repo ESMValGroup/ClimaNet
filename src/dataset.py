bdf6ff11a606544eb568b1ff4e0736619995474d
Ou Ku <o.ku@esciencecenter.nl>
2026-02-11 09:11:52 +0100

    update dataset

--- src/dataset.py
+++ src/dataset.py
@@ -2,7 +2,7 @@ import xarray as xr
 import numpy as np
 import torch
 from torch.utils.data import Dataset
-from typing import Tuple, Optional, Dict
+from typing import Tuple
 
 
 class SSTDataset(Dataset):
@@ -12,24 +12,20 @@ class SSTDataset(Dataset):
         self,
         daily_ds: xr.Dataset,
         monthly_ds: xr.Dataset,
-        mask_ds: xr.Dataset = None,
+        mask_da: xr.DataArray = None,
         daily_var: str = "ts",
         monthly_var: str = "ts",
         mask_var: str = "lsm",
         patch_size: Tuple[int, int] = (16, 16),
         overlap: int = 0,
-        spatial_subset: Optional[Dict[str, slice]] = None,
-        transform=None,
     ):
         self.daily_ds = daily_ds
         self.monthly_ds = monthly_ds
-        self.mask_ds = mask_ds
+        self.mask_da = mask_da
         self.daily_var = daily_var
         self.monthly_var = monthly_var
-        self.mask_var = mask_var
         self.patch_size = patch_size
         self.overlap = overlap
-        self.transform = transform
 
         # Precompute lazy index mapping for patches
         lat_dim = self.daily_ds.sizes["lat"]
@@ -37,14 +33,19 @@ class SSTDataset(Dataset):
         self.time_dim = self.monthly_ds.sizes["time"]  # Use monthly for samples
 
         self.stride = self.patch_size[0] - self.overlap
-        self.n_i = (lat_dim - self.patch_size[0]) // self.stride + 1
-        self.n_j = (lon_dim - self.patch_size[1]) // self.stride + 1
+        self.n_i = (
+            lat_dim - self.patch_size[0]
+        ) // self.stride + 1  # number of horizontal patches
+        self.n_j = (
+            lon_dim - self.patch_size[1]
+        ) // self.stride + 1  # number of vertical patches
         self.total_len = self.time_dim * self.n_i * self.n_j
 
     def __len__(self):
         return self.total_len
 
     def __getitem__(self, idx):
+        """Get a spatiotemporal patch sample based on the index."""
         if idx < 0 or idx >= self.total_len:
             raise IndexError("Index out of range")
 
@@ -63,6 +64,8 @@ class SSTDataset(Dataset):
         daily_patch = (
             self.daily_ds[self.daily_var].isel(lat=lat_slice, lon=lon_slice).to_numpy()
         )
+        # Add a band dimension for daily data to accommodate model input shape
+        daily_patch = daily_patch[np.newaxis, ...]
 
         # Get monthly target
         monthly_patch = (
@@ -70,14 +73,12 @@ class SSTDataset(Dataset):
             .isel(time=t, lat=lat_slice, lon=lon_slice)
             .to_numpy()
         )
+        # Add a band dimension for monthly data
+        monthly_patch = monthly_patch[np.newaxis, ...]
 
         # Get mask patch if available
-        if self.mask_ds is not None:
-            mask_patch = (
-                self.mask_ds[self.mask_var]
-                .isel(lat=lat_slice, lon=lon_slice)
-                .to_numpy()
-            )
+        if self.mask_da is not None:
+            mask_patch = self.mask_da.isel(lat=lat_slice, lon=lon_slice).to_numpy()
         else:
             mask_patch = np.ones_like(monthly_patch.isel(time=0), dtype=bool)
 
@@ -88,8 +89,5 @@ class SSTDataset(Dataset):
             "mask": torch.from_numpy(mask_patch).bool(),
             "coords": (t, i, j),
         }
-
-        if self.transform:
-            sample = self.transform(sample)
 
         return sample
