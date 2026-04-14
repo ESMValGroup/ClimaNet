95ad60ade3efcc2592787b09623033d312c78796
SarahAlidoost <f.alidoost@esciencecenter.nl>
2026-04-14 15:11:29 +0200

    fix linter errors

--- climanet/__init__.py
+++ climanet/__init__.py
@@ -2,8 +2,4 @@ from climanet import st_encoder_decoder
 from climanet.dataset import STDataset
 from climanet.utils import regrid_to_boundary_centered_grid
 
-__all__ = [
-    "STDataset",
-    "st_encoder_decoder",
-    "regrid_to_boundary_centered_grid"
-]
+__all__ = ["STDataset", "st_encoder_decoder", "regrid_to_boundary_centered_grid"]
