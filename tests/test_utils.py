d9721dfd2b971b06d77c55ad2761974c203df1a7
Ou Ku <o.ku@esciencecenter.nl>
2026-06-10 11:46:38 +0200

    add unit test for writer

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,2 +1,11 @@
-def test_dummy():
-    assert True
+from climanet.utils import setup_logging
+
+
+def test_setup_logging(tmp_path):
+    writer = setup_logging(str(tmp_path))
+    writer.add_text("test", "This is a test log entry.")
+    writer.close()
+
+    # Test that there is at least one events file
+    # The filename should have "UTC" keyword, which is the timestamp suffix
+    assert any(tmp_path.glob("events.out.*UTC*"))
