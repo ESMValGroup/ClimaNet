6abae86300a6d9dbe091c449c182bc457eca0f76
SarahAlidoost <f.alidoost@esciencecenter.nl>
2026-07-31 15:41:24 +0200

    fix ruff

--- scripts/data_preparation.py
+++ scripts/data_preparation.py
@@ -1,7 +1,8 @@
 import argparse
 from pathlib import Path
-from dask.distributed import Client, LocalCluster
+
 import xarray as xr
+from dask.distributed import Client, LocalCluster
 
 from climanet.utils import data_preparation, data_split
 
